5f31a38c5f8ba0c2fd6735255fd4d79a4343ae80
Ronnen Nagal <nagalr@gmail.com>
2020-08-30 15:16:08 +0300

    Create the UserDto class

---
+++ src/main/java/com/photoappapiusers/photoapp/api/users/service/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package com.photoappapiusers.photoapp.api.users.service;
+
+import com.photoappapiusers.photoapp.api.users.shared.UserDto;
+
+public class UserServiceImpl implements UsersService {
+
+    @Override
+    public UserDto createUser(UserDto userDetails) {
+        return null;
+    }
+
+}
---
+++ src/main/java/com/photoappapiusers/photoapp/api/users/service/UsersService.java
@@ -0,0 +1,9 @@
+package com.photoappapiusers.photoapp.api.users.service;
+
+import com.photoappapiusers.photoapp.api.users.shared.UserDto;
+
+public interface UsersService {
+
+    UserDto createUser(UserDto userDetails);
+
+}
