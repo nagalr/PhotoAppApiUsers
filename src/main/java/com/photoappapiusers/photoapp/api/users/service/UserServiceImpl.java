d35369aca4bd3da60c4881e0b9752590dcfc2236
Ronnen Nagal <nagalr@gmail.com>
2020-08-30 15:23:34 +0300

    Generate UserId with UUID

--- src/main/java/com/photoappapiusers/photoapp/api/users/service/UserServiceImpl.java
+++ src/main/java/com/photoappapiusers/photoapp/api/users/service/UserServiceImpl.java
@@ -2,10 +2,16 @@ package com.photoappapiusers.photoapp.api.users.service;
 
 import com.photoappapiusers.photoapp.api.users.shared.UserDto;
 
+import java.util.UUID;
+
 public class UserServiceImpl implements UsersService {
 
     @Override
     public UserDto createUser(UserDto userDetails) {
+
+        userDetails.setUserId(UUID.randomUUID().toString());
+
+
         return null;
     }
 
